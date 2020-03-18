727854508c7eb0f353f81443201ad9ce5371a17e
Sajal Sood <sajal.sood@gmail.com>
2020-03-18 17:18:21 -0400

    Cloud Watch

--- script.sh
+++ script.sh
@@ -14,3 +14,7 @@ sudo apt-get install ruby -y
 sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
+sudo apt update
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo chmod +x ./amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
